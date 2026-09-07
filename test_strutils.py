e23a862211021b9ca7bfe5158d003403616c4a4b
leonistuff67-commits <leonistuff67@gmail.com>
2026-09-06 20:13:17 -0500

    Add missing test for exact-length boundary in truncate() (#1)

--- test_strutils.py
+++ test_strutils.py
@@ -7,3 +7,7 @@ def test_no_truncation_needed():
 
 def test_truncates_long_text():
     assert truncate("hello world", 8) == "hello..."
+
+
+def test_exact_length_is_not_truncated():
+    assert truncate("hello", 5) == "hello"
